e3f50022659b5eb1bca152690aa926217aa6b845
Dmytro Velychko <dmytro.velychko@gmail.com>
2020-07-02 00:47:09 +0200

    Added pytorch benchmark

---
+++ mmtorch.py
@@ -0,0 +1,46 @@
+import torch
+torch.set_default_dtype(torch.float64)
+start = torch.cuda.Event(enable_timing=True)
+end = torch.cuda.Event(enable_timing=True)
+n = 10000
+x = torch.randn(n, n)
+y = torch.randn(n, n)
+start.record()
+z = torch.mm(x, y)
+end.record()
+torch.cuda.synchronize()
+num_ops = 2*n**3-n**2
+print(num_ops / start.elapsed_time(end) / 10**6, "GFLOPS CPU double")
+
+
+import torch
+torch.set_default_dtype(torch.float32)
+device = torch.cuda.current_device()
+start = torch.cuda.Event(enable_timing=True)
+end = torch.cuda.Event(enable_timing=True)
+n = 10000
+x = torch.randn(n, n).to(device)
+y = torch.randn(n, n).to(device)
+start.record()
+z = torch.mm(x, y)
+end.record()
+torch.cuda.synchronize()
+num_ops = 2*n**3-n**2
+print(num_ops / start.elapsed_time(end) / 10**6, "GFLOPS CUDA single")
+
+
+import torch
+torch.set_default_dtype(torch.float64)
+device = torch.cuda.current_device()
+start = torch.cuda.Event(enable_timing=True)
+end = torch.cuda.Event(enable_timing=True)
+n = 10000
+x = torch.randn(n, n).to(device)
+y = torch.randn(n, n).to(device)
+start.record()
+z = torch.mm(x, y)
+end.record()
+torch.cuda.synchronize()
+num_ops = 2*n**3-n**2
+print(num_ops / start.elapsed_time(end) / 10**6, "GFLOPS CUDA double")
+
