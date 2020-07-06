823bc6c36cc655dff1ba985985143c4f397ff843
Dmytro Velychko <dmytro.velychko@gmail.com>
2020-07-06 21:11:14 +0200

    Added many operations

--- mmtorch.py
+++ mmtorch.py
@@ -1,16 +1,25 @@
 import torch
+
+def op(a, b):
+    #return torch.logdet(a)
+    #return torch.inverse(a)
+    #return torch.eig(a)
+    return torch.cholesky(torch.mm(a, a.t()))
+    #return torch.mm(a, b)
+
 torch.set_default_dtype(torch.float64)
 start = torch.cuda.Event(enable_timing=True)
 end = torch.cuda.Event(enable_timing=True)
-n = 1000
+n = 1024*4
 x = torch.randn(n, n)
 y = torch.randn(n, n)
-start.record()
-z = torch.mm(x, y)
-end.record()
-torch.cuda.synchronize()
-num_ops = 2*n**3-n**2
-print(num_ops / start.elapsed_time(end) / 10**6, "GFLOPS CPU double")
+for i in range(2):
+    start.record()
+    z = op(x, y)
+    end.record()
+    torch.cuda.synchronize()
+    num_ops = 2*n**3-n**2
+    print(num_ops / start.elapsed_time(end) / 10**6, "GFLOPS CPU double")
 
 
 import torch
@@ -18,12 +27,12 @@ torch.set_default_dtype(torch.float32)
 device = torch.cuda.current_device()
 start = torch.cuda.Event(enable_timing=True)
 end = torch.cuda.Event(enable_timing=True)
-n = 10000
+n = 1024*1
 x = torch.randn(n, n).to(device)
 y = torch.randn(n, n).to(device)
-for i in range(10):
+for i in range(2):
     start.record()
-    z = torch.mm(x, y)
+    z = op(x, y)
     end.record()
     torch.cuda.synchronize()
     num_ops = 2*n**3-n**2
@@ -38,10 +47,11 @@ end = torch.cuda.Event(enable_timing=True)
 n = 1024*4
 x = torch.randn(n, n).to(device)
 y = torch.randn(n, n).to(device)
-for i in range(10):
+for i in range(2):
     start.record()
-    z = torch.mm(x, y)
+    z = op(x, y)
     end.record()
     torch.cuda.synchronize()
     num_ops = 2*n**3-n**2
     print(num_ops / start.elapsed_time(end) / 10**6, "GFLOPS CUDA double")
+
